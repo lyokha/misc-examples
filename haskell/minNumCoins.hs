12b79f969d938f8b78b0192465e283dc81fc7003
lyokha <alexey.radkov@gmail.com>
2015-07-31 19:24:01 +0300

    added even faster minNumCoinsVectorBasic

--- haskell/minNumCoins.hs
+++ haskell/minNumCoins.hs
@@ -1,15 +1,16 @@
 -- compile hint: ghc --make -O2 -fforce-recomp -fllvm -optlo-O3 minNumCoins.hs
 -- for criterion benchmarking use option -b
 
-import Data.Function.Memoize (memoFix2)
-import Data.IntMap.Strict (singleton, insert, split, (!))
-import Data.List (mapAccumL, sortBy)
-import Data.Array.IO
-import Data.Array.Base (unsafeRead, unsafeWrite)
-import Control.Monad (foldM, liftM)
+import           Data.Function.Memoize       (memoFix2)
+import           Data.IntMap.Strict          (singleton, insert, split, (!))
+import           Data.List                   (mapAccumL, sortBy)
+import           Data.Array.IO
+import           Data.Array.Base             (unsafeRead, unsafeWrite)
+import           Control.Monad               (foldM, foldM_, liftM)
 import qualified Data.Vector.Unboxed.Mutable as V
-import Criterion.Main
-import System.Environment
+import qualified Data.Vector.Unboxed         as VI
+import           Criterion.Main
+import           System.Environment
 
 minNumCoinsPlain :: [Int] -> Int -> Int
 minNumCoinsPlain c m = map (mnc c) [0 .. m] !! m
@@ -43,15 +44,13 @@ minNumCoinsMap c m = snd (mapAccumL (mnc c) (singleton 0 0) [0 .. m]) !! m
                              in (insert m cur (snd $ split (m - mc) s), cur)
                     where mc = maximum c
 
-type Storage = IOUArray Int Int
-
 minNumCoinsArray :: [Int] -> Int -> IO Int
 minNumCoinsArray c m = do
-    a <- newArray (0, m) 0 :: IO Storage
+    a <- newArray (0, m) 0 :: IO (IOUArray Int Int)
     mapM_ (mnc cs a) [1 .. m]
     unsafeRead a m
     where cs = sortBy (flip compare) c
-          mnc :: [Int] -> Storage -> Int -> IO ()
+          mnc :: [Int] -> IOUArray Int Int -> Int -> IO ()
           mnc c a m = foldM (step a) m (dropWhile (> m) c) >>= unsafeWrite a m
             where step a b x = liftM (min b . succ) $ unsafeRead a $ m - x
 
@@ -65,17 +64,36 @@ minNumCoinsVector c m = do
           mnc c v m = foldM (step v) m (dropWhile (> m) c) >>= V.unsafeWrite v m
             where step v a x = liftM (min a . succ) $ V.unsafeRead v $ m - x
 
+minNumCoinsVectorBasic :: VI.Vector Int -> Int -> IO Int
+minNumCoinsVectorBasic c m = do
+    v <- V.new $ m + 1
+    V.unsafeWrite v 0 0
+    mapM_ (mnc c v) [1 .. m]
+    V.unsafeRead v m
+    where mnc c v m = VI.foldM_ (step v) m c
+            where step v a x =
+                    if x > m
+                        then return a
+                        else do y <- V.unsafeRead v $ m - x
+                                let cur = y + 1
+                                if cur < a
+                                    then do V.unsafeWrite v m cur
+                                            return cur
+                                    else return a
+
 main = do
     args <- getArgs
     if benchArg `elem` args
         then withArgs (filter (/= benchArg) args) $ defaultMain
              [
-                 bench ("map " ++ show m2)    $ whnf
-                     (minNumCoinsMap coins) m2,
-                 bench ("array " ++ show m2)  $ whnfIO $
-                     minNumCoinsArray coins m2,
-                 bench ("vector " ++ show m2) $ whnfIO $
-                     minNumCoinsVector coins  m2
+                 bench ("map " ++ show m2)          $ whnf
+                     (minNumCoinsMap        coins) m2,
+                 bench ("array " ++ show m2)        $ whnfIO $
+                     minNumCoinsArray       coins  m2,
+                 bench ("vector " ++ show m2)       $ whnfIO $
+                     minNumCoinsVector      coins  m2,
+                 bench ("vector basic " ++ show m2) $ whnfIO $
+                     minNumCoinsVectorBasic vcoins m2
              ]
         else {-print $ minNumCoinsPlain [1, 2, 5] 20   -- m = 50 will hang it!-}
              {-print $ minNumCoins_     [1, 2, 5] 20   -- m = 50 will hang it!-}
@@ -86,9 +104,10 @@ main = do
              {-print $ minNumCoinsMap   coins m1-}
              {-print $ minNumCoinsMap   coins m2-}
              {-minNumCoinsArray         coins m2 >>= print-}
-             minNumCoinsVector        coins m2 >>= print
+             minNumCoinsVectorBasic   vcoins m2 >>= print
     where benchArg = "-b"
           coins    = [24, 18, 14, 11, 10, 8, 5, 3, 1]
+          vcoins   = VI.fromList $ sortBy (flip compare) coins
           m1       = 16659
           m2       = 1665900
 
