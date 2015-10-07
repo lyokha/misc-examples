5b99d2809fa9269d6bdd5ee0ea8ffec6e8b42075
lyokha <alexey.radkov@gmail.com>
2015-10-07 12:57:21 +0300

    added a new haskell example

---
+++ haskell/SuperscriptAlias.hs
@@ -0,0 +1,17 @@
+module Numeric.MathFunction.SuperscriptAlias
+    ( sin², sin³
+    , cos², cos³
+    , tan², tan³
+    , pi²
+    ) where
+
+f *^ x =  (^ x) . f
+
+sin² = sin      *^ 2
+sin³ = sin      *^ 3
+cos² = cos      *^ 2
+cos³ = cos      *^ 3
+tan² = tan      *^ 2
+tan³ = tan      *^ 3
+pi²  = const pi *^ 2 $ Nothing  -- can also be defined as simple as pi ^ 2
+
