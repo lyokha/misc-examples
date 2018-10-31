70090a5e0742eb31d193965a0cdbe6db724d3dbe
lyokha <alexey.radkov@gmail.com>
2018-10-31 16:20:29 +0300

    minor stylistic improvements

--- haskell/shortest_path/tsp.m
+++ haskell/shortest_path/tsp.m
@@ -3,7 +3,7 @@ x = tsp(:,1);
 y = tsp(:,2);
 figure(1);
 plot(x, y, 'o');
-#saveas(1, "tsp.png");
+%saveas(1, "tsp.png");
 title("Original set");
 
 load tsp_res.dat
@@ -11,7 +11,7 @@ x1 = tsp_res(:,1);
 y1 = tsp_res(:,2);
 figure(2);
 plot(x1, y1, '-o');
-#saveas(2, "tsp_res.png");
+%saveas(2, "tsp_res.png");
 title("Shortest path");
 
 load tsp_res_r.dat
@@ -19,7 +19,7 @@ x2 = tsp_res_r(:,1);
 y2 = tsp_res_r(:,2);
 figure(3);
 plot(x2, y2, '-o');
-#saveas(3, "tsp_res_r.png");
+%saveas(3, "tsp_res_r.png");
 title("Shortest path (reversed)");
 
 load tsp_res_p.dat
@@ -27,7 +27,7 @@ x3 = tsp_res_p(:,1);
 y3 = tsp_res_p(:,2);
 figure(4);
 plot(x3, y3, '-o');
-#saveas(4, "tsp_res_p.png");
+%saveas(4, "tsp_res_p.png");
 title("Shortest path (parallel)");
 
 load tsp_res_pr.dat
@@ -35,7 +35,7 @@ x4 = tsp_res_pr(:,1);
 y4 = tsp_res_pr(:,2);
 figure(5);
 plot(x4, y4, '-o');
-#saveas(5, "tsp_res_pr.png");
+%saveas(5, "tsp_res_pr.png");
 title("Shortest path (parallel reversed)");
 
 pause()
